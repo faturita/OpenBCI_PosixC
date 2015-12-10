7ee50218fce27c87fac8e0314b0ba62fa9dc834d
Rodrigo Ramele <rramele@gmail.com>
2015-12-10 00:12:36 -0300

    Log sampled data into a file.

--- openbciserial.cpp
+++ openbciserial.cpp
@@ -1,3 +1,31 @@
+/**
+Header
+Byte 0: 0xA0
+Bytes 1: Packet counter
+
+EEG Data
+Note: values are 24-bit unsigned int, MSB first
+
+Bytes 2-4: Data value for EEG channel 0
+Bytes 5-7: Data value for EEG channel 1
+Bytes 8-10: Data value for EEG channel 2
+Bytes 11-13: Data value for EEG channel 3
+Bytes 14-16: Data value for EEG channel 4
+Bytes 17-19: Data value for EEG channel 5
+Bytes 20-22: Data value for EEG channel 6
+Bytes 23-25: Data value for EEG channel 7
+
+Accelerometer Data
+Note: values are 16-bit int, MSB first
+
+Bytes 26-27: Data value for accelerometer channel X
+Bytes 28-29: Data value for accelerometer channel Y
+Bytes 30-31: Data value for accelerometer channel Z
+
+Footer
+Byte 32: 0xC0
+**/
+
 #include <stdio.h>
 #include <errno.h>
 #include <termios.h>
@@ -69,21 +97,25 @@ set_blocking (int fd, int should_block)
 int cont=1;
 
 
-void my_handler(sig_t s){
-           printf("Caught signal %d\n",s);
-           if (cont==0) _exit(0);
-           cont=0;
+void killsignalhandler(sig_t s){
+    printf("Caught signal %d\n",s);
+    if (cont==0)
+    {
+        _exit(0);
+    }
+    cont=0;
 }
 
 
+// Use me to retrieve one frame.
 int readblock(int fd, char *eeg, size_t size)
 {
     char c;
     static char previousC;
 
-    int seq=0;
+    unsigned long seq=0;
 
-    memset(eeg,0,sizeof(eeg));
+    memset((void *)eeg,0,sizeof(eeg));
 
     int n = read (fd, &c, 1);
     while (cont && n>=0)
@@ -97,7 +129,7 @@ int readblock(int fd, char *eeg, size_t size)
 
             sprintf(header,"%d",(unsigned int)c );
 
-            //printf("<%d>", c);
+            printf("<%u>", c);
 
             if (seq>0)
             {
@@ -119,7 +151,7 @@ int readblock(int fd, char *eeg, size_t size)
 
         n = read (fd, &c, 1);
     }
-    return 0;
+    return n;
 }
 
 
@@ -138,121 +170,169 @@ int interpret24bitAsInt32(char *eeg) {
   }
 
 
-int test()
+int openserialport(char *portname)
 {
-    char *portname = "/dev/tty.usbserial-DN0096Q1";
-
-    int n;
-
-    signal (SIGINT,(void (*)(int))my_handler);
-
-    printf ("Starting...\n");
+    printf ("Opening Serial Port %s.\n", portname);
 
     //int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
     int fd = open (portname, O_RDWR | O_NOCTTY | O_NDELAY);
     if (fd < 0)
     {
             printf ("error %d opening %s: %s", errno, portname, strerror (errno));
-            return 2;
+            return fd;
     }
 
-    usleep ((7 + 25) * 100000);             // sleep enough to transmit the 7 plus
+    usleep ((7 + 25) * 100000);             // sleep just a bit.
 
-    printf ("USB Serial Port Open\n");
+    printf ("USB Serial Port Open.\n");
 
     set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
     //set_blocking (fd, 0);                // set no blocking
 
     printf ("Waiting....\n");
-    usleep ((7 + 25) * 100000);             // sleep enough to transmit the 7 plus
+    usleep ((7 + 25) * 100000);             // sleep enough a little more.
 
+    return fd;
+}
 
-    printf ("Sending command\n");
-
+int beginstreamingdata(int fd)
+{
     char buf [256];
 
-    write (fd, "?", 1);           // send 7 character greeting
+    ssize_t nbytes = write (fd, "?", 1);           // send the register query command.
+    if (nbytes==-1)
+    {
+        printf ("error %d writing bytes to port: %s", errno, strerror (errno));
+        return -1;
+    }
 
-    usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
-                                     // receive 25:  approx 100 uS per char transmit
 
-    n = read (fd, buf, sizeof(buf));  // read up to 100 characters if ready to read
+    usleep ((7 + 25) * 100);             // sleep enough to transmit those bytes.
 
-    printf ("%d bytes received\n", n);
+    int n = read (fd, buf, sizeof(buf));  // read up to 100 characters if ready to read
 
-    int timeout = 2500000;
+    //printf ("%d bytes received\n", n);
 
-    while (cont)
+    int stopMark = 0;  // $ counter
+
+    while (true)
     {
         //printf ("Reading:%d\n",n);
         for(int i=0;i<n;i++)
         {
             char c = buf[i];
             printf ("%c",c);
+
+            if (c=='$')
+                stopMark++;
         }
-        timeout--;
-        n = read (fd, buf, sizeof(buf));  // read up to 100 characters if ready to read
+
+        if (stopMark>=3)
+            break;
+
+        n = read (fd, buf, sizeof(buf));  // read up to Buf characters if ready to read
     }
 
-    write (fd, "b", 1);           // send 7 character greeting
+    nbytes = write (fd, "b", 1);           // send the b command to start streaming.
+    if (nbytes == -1)
+    {
+        printf ("error %d writing bytes to port: %s", errno, strerror (errno));
+        return -1;
+    }
 
-    usleep ((7 + 25) * 1000);             // sleep enough to transmit the 7 plus
+    usleep ((7 + 25) * 1000);             // sleep enough to transmit the command
 
+    return 0;
+}
+
+int logstreamingdata(int fd, char * filename, int duration)
+{
     char eeg[35];
+    int fs=512;
+
+    int samples=1;
+    int n;
 
     cont=1;
 
+    FILE *pf = fopen(filename, "w");
+
     n = readblock (fd, eeg, 33);
-    while (cont)
+    while (cont && (samples <= (fs*duration))  )
     {
         if (n>0)
         {
-            printf ("[%d][%d][%d][%d][%d]\n", eeg[0],eeg[0+1],eeg[32], eeg[26],eeg[27]);
+            int x = (
+                ((0xFF & eeg[26]) << 8) |
+                (0xFF & eeg[27])
+              );
 
-            int x=(int)(( (int)eeg[26] << 8) || eeg[27]);
-            int y=(int)(( (int)eeg[28] << 8) || eeg[29]);
-            int z=(int)(( (int)eeg[30] << 8) || eeg[31]);
+            int y = (
+                ((0xFF & eeg[28]) << 8) |
+                (0xFF & eeg[29])
+              );
 
-            printf("(EEG: %d)(X:%d)(Y:%d)(Z:%d)\n", interpret24bitAsInt32(eeg+23), x,y,z);
+            int z = (
+                ((0xFF & eeg[30]) << 8) |
+                (0xFF & eeg[31])
+              );
+
+            int channel0 = interpret24bitAsInt32(eeg+2);
+            int channel1 = interpret24bitAsInt32(eeg+5);
+            int channel2 = interpret24bitAsInt32(eeg+8);
+            int channel3 = interpret24bitAsInt32(eeg+11);
+            int channel4 = interpret24bitAsInt32(eeg+14);
+            int channel5 = interpret24bitAsInt32(eeg+17);
+            int channel6 = interpret24bitAsInt32(eeg+20);
+            int channel7 = interpret24bitAsInt32(eeg+23);
+
+            printf("(EEG: %d)(X:%d)(Y:%d)(Z:%d)\n", channel7 , x,y,z);
+
+            fprintf(pf,"%d %d %d %d %d %d %d %d %d %d %d %d\n", (int)eeg[1]+128, x,y,z,channel0,channel1,channel2,channel3, channel4, channel5, channel6, channel7);
+
         }
 
         n = readblock (fd, eeg, 33);
+        samples++;
     }
 
-    write (fd, "s", 1);           // send 7 character greeting
+    fclose(pf);
+    return 0;
+}
+
+int endstreamingdata(int fd)
+{
+    // Ready to receive command info.
+
+    ssize_t nbytes = write (fd, "s", 1);
+    if (nbytes == -1)
+    {
+        printf ("error %d writing bytes to port: %s", errno, strerror (errno));
+        return -1;
+    }
 
     usleep ((7 + 25) * 100);             // sleep enough to transmit the 7 plus
+
+    return 0;
+}
+
+
+int test()
+{
+    char *portname = "/dev/tty.usbserial-DN0096Q1";
+
+    signal (SIGINT,(void (*)(int))killsignalhandler);
+
+    int fd = openserialport(portname);
+
+    beginstreamingdata(fd);
+
+    logstreamingdata(fd, "eeg.dat", 10);
+
+    endstreamingdata(fd);
 
     close(fd);
 
     return 1;
 }
 
-
-/**
-Header
-Byte 0: 0xA0
-Bytes 1: Packet counter
-
-EEG Data
-Note: values are 24-bit unsigned int, MSB first
-
-Bytes 2-4: Data value for EEG channel 0
-Bytes 5-7: Data value for EEG channel 1
-Bytes 8-10: Data value for EEG channel 2
-Bytes 11-13: Data value for EEG channel 3
-Bytes 14-16: Data value for EEG channel 4
-Bytes 17-19: Data value for EEG channel 5
-Bytes 20-22: Data value for EEG channel 6
-Bytes 23-25: Data value for EEG channel 7
-
-Accelerometer Data
-Note: values are 16-bit int, MSB first
-
-Bytes 26-27: Data value for accelerometer channel X
-Bytes 28-29: Data value for accelerometer channel Y
-Bytes 30-31: Data value for accelerometer channel Z
-
-Footer
-Byte 32: 0xC0
-**/
